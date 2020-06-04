bf2bcad8caeccbc545f4821f7f3c42fc7520a5d3
Oleg Lozovatyi <lozovatyi.jr@gmail.com>
2020-06-04 16:50:35 -0400

    added more content and styling, cleaned up the code

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    
     // energy variable
     let energy = 100;
     $('.energy').html(`<p>Your Energy: ${energy}%</p>`);
@@ -37,13 +38,26 @@ $(document).ready(function(){
     }
     displayCreature(index++);
 
+    // winning and losing conditions
+    const win = () => {
+        if (index === creatures.length + 1 && energy > 0) {
+            window.location.href = './winPage.html';
+        }
+    }
+
+    const lose = () => {
+        if (energy === 0) {
+            window.location.href = './losePage.html';
+        } else {
+            displayCreature(index++);
+        }
+    }
+
     // correct answer functionality
     $('.correct').on('click', function() {
         setTimeout(function() {
             displayCreature(index++);
-            if (index === creatures.length + 1 && energy > 0) {
-                window.location.href = './winPage.html';
-            }
+            win();
         }, 800);
     })
     
@@ -52,15 +66,8 @@ $(document).ready(function(){
         energy -= 50;
         $('.energy').html(`<p>Your Energy: ${energy}%</p>`);
         setTimeout(function() {
-            if (energy === 0) {
-                window.location.href = './losePage.html';
-            } else {
-                displayCreature(index++);
-            }
-            if (index === creatures.length + 1 && energy >= 0) {
-                window.location.href = './winPage.html';
-            }
+            lose();
+            win();
         }, 800);
     })
-
 });
